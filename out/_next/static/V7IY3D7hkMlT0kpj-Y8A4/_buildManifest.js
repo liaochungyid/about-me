b8e164e23f049b1db98f29948694b409057b663d
liaochungyi <lyle.liao@pinnov.com>
2022-10-31 00:18:25 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/V7IY3D7hkMlT0kpj-Y8A4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-eb52ecfdfcbfe2ff.js","static/chunks/pages/index-699457d78a881a27.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
